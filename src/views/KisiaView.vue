b98a78ad4ac9ea6bdd25d92944ffa9d02da929dd
SunM <coex0113@gmail.com>
2024-08-21 00:23:33 +0900

    KisiaView 로 수정하기 명칭자꾸

--- src/views/KisiaView.vue
+++ src/views/KisiaView.vue
@@ -2,19 +2,17 @@
   <!-- <side-nav class="w-1/6 float-left"></side-nav> -->
   <div class="w-full float-right px-5">
     <header-breadcrumb></header-breadcrumb>
-    <main class="scroll-h scroll overflow-auto rounded-lg">
+    <cycle-loading
+      v-if="loading"></cycle-loading>
+    <main class="scroll-h scroll overflow-auto rounded-lg"
+      v-else-if="!loading && isApiOk">
       <Suspense>
       <register-main 
         v-if="responseData"
         :responseData="responseData"></register-main>
       </Suspense>
-
-      <detection-count
-        :detectionFileCount="detectionFileCount"></detection-count>
-      <file-details
-        :fileDetails="fileDetails"></file-details>
-      <!-- <content-error v-else></content-error> -->
     </main>
+    <content-error v-else></content-error>
   </div>
 </template>
 
@@ -31,10 +29,12 @@ import ContentError from '@/components/ContentError.vue'
 import { getSaasListApi } from '@/apis/register.js'
 import { fileScanApi } from '@/apis/file.js'
 
-let loading = ref(false);
+let loading = ref(true);
+let isApiOk = ref(false);
+
 let responseData = ref(null);
 let error = ref(null);
-let orgId = 1;
+let orgId = 3;
 
 responseData.value = getSaasListApi(orgId);
 
@@ -42,7 +42,7 @@ axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
 
 const fetchPosts = async (orgId) => {
   try {
-    const response = await axios.get('/api/v1/org-saas/1');
+    const response = await axios.post('/api/v1/org-saas/3');
     if(response.status == '200') {
       return await response.data;
     }
@@ -60,12 +60,12 @@ Promise.all([
   ]).then((values) => {
     console.log('fileScan',values[0]);
     fileDetails.value = values[0]
-  detectionFileCount.value = [values[0].data.total, values[0].data.dlpTotal, values[0].data.malwareTotal];
-  // isApiOk.value = true;
+    detectionFileCount.value = [values[0].data.total, values[0].data.dlpTotal, values[0].data.malwareTotal];
+    isApiOk.value = true;
 }).catch((err) => {
   console.log(err);
 }).finally(() => {
-  // loading.value = false;
+  loading.value = false;
 });
 
 </script>
