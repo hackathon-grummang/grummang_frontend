5b8e3e4c1987ed0a7fd96370fc0626aff4082fd6
SunM <coex0113@gmail.com>
2024-08-20 17:08:21 +0900

    Footer 지우기, ErrorView 부분 수정

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full float-right px-5">
+    <header-breadcrumb></header-breadcrumb>
+    <content-error></content-error>
+  </div>
+  <!-- <footer>
+    <the-footer></the-footer>
+  </footer> -->
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import HeaderBreadcrumb from '@/components/HeaderBreadcrumb.vue'
+// import TheFooter from '@/components/TheFooter.vue'
+import ContentError from '@/components/ContentError.vue'
+import CycleLoading from '@/components/CycleLoading.vue'
+
+let loading = ref(true);
+let isApiOk = ref(false);
+
+Promise.all([
+]).then((values) => {
+  isApiOk.value = false;
+}).catch((err) => {
+  console.log(err);
+}).finally(() => {
+  loading.value = false;
+});
+
+</script>
