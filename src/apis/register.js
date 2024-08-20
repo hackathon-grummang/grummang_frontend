85643e080c9d97764a3316b7ac2dc1f96ab382a5
SunM <coex0113@gmail.com>
2024-08-21 05:36:14 +0900

    Register Api BaseUrl 변동

--- src/apis/register.js
+++ src/apis/register.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_HACKATHON_API;
 
 let getSaasListApi = async (orgId) => {
   try {
