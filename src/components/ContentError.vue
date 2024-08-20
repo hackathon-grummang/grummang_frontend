3d5c3e02ebb7983d0a49a05c24ac51e5b5678d8e
SunM <coex0113@gmail.com>
2024-08-20 17:04:40 +0900

    Content Error 부분 수정

--- src/components/ContentError.vue
+++ src/components/ContentError.vue
@@ -5,8 +5,8 @@
     <v-icon size="140" color="#FF9A00">mdi-cloud-alert-outline</v-icon>
     <h1 class="mt-2 text-xl font-bold tracking-tight text-gray-900">현재 페이지를 불러올 수 없습니다.</h1>
 
-    <p class="mt-2 text-sm text-gray-900">정보를 불러오는데 문제가 발생했습니다.</p>
-    <p class="mt-5 text-base font-semibold text-orange-500 italic">새로고침 해보시거나, 문제가 지속적으로 발생하면은 제조사에게 문의해주세요.</p>
+    <p class="mt-2 text-base text-orange">이 페이지는 오류 페이지입니다.</p>
+    <!-- <p class="mt-5 text-base font-semibold text-orange-500 italic">새로고침 해보시거나, 문제가 지속적으로 발생하면은 제조사에게 문의해주세요.</p> -->
   </div>
 </div>
 </template>
